2a673423002faf617c1dd76eebd1b3d1757e481f
Ahsan Barkati <ahsanbarkati@gmail.com>
2020-11-12 16:04:12 +0530

    make generator faster

--- cmd/data-generator/main.go
+++ cmd/data-generator/main.go
@@ -7,16 +7,18 @@ import (
 	"math"
 	"math/rand"
 	"os"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/linuxerwang/dgraph-bench/tasks"
 )
 
 const (
-	maxUid           = 10000000
-	maxDirectFriends = 1000
-
-	k = 100
+	maxUid           = 1e6
+	maxDirectFriends = 300
+	chunkSize        = int(maxUid / 50)
+	k                = 100
 )
 
 var (
@@ -29,15 +31,24 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
+	var buf strings.Builder
 	w := gzip.NewWriter(f)
+	s := rand.NewSource(42)
+	r := rand.New(s)
+	start := time.Now()
+
+	ch := make(chan []byte, 16)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go worker(w, ch, &wg)
+
 	for i := 1; i <= maxUid; i++ {
 		meID := fmt.Sprintf("_:m.%d", i)
-		writeNQuad(w, meID, "xid", fmt.Sprintf("\"%d\"", i))
-		writeNQuad(w, meID, "name", fmt.Sprintf("\"%s\"", tasks.RandString(10)))
-		writeNQuad(w, meID, "age", fmt.Sprintf("\"%d\"", 18+rand.Intn(80)))
-		writeNQuad(w, meID, "created_at", fmt.Sprintf("\"%d\"", time.Now().UnixNano()))
-		writeNQuad(w, meID, "updated_at", fmt.Sprintf("\"%d\"", time.Now().UnixNano()))
+		buf.WriteString(getNQuad(meID, "xid", fmt.Sprintf("\"%d\"", i)))
+		buf.WriteString(getNQuad(meID, "name", fmt.Sprintf("\"%s\"", tasks.RandString(10, r))))
+		buf.WriteString(getNQuad(meID, "age", fmt.Sprintf("\"%d\"", 18+rand.Intn(80))))
+		buf.WriteString(getNQuad(meID, "created_at", fmt.Sprintf("\"%d\"", time.Now().UnixNano())))
+		buf.WriteString(getNQuad(meID, "updated_at", fmt.Sprintf("\"%d\"", time.Now().UnixNano())))
 
 		friendCnt := randomNum()
 		for j := 1; j <= friendCnt; j++ {
@@ -45,10 +56,21 @@ func main() {
 			for fID == i {
 				fID = rand.Intn(maxUid)
 			}
-			writeNQuad(w, meID, "friend_of", fmt.Sprintf("<_:m.%d>", fID))
+			buf.WriteString(getNQuad(meID, "friend_of", fmt.Sprintf("<_:m.%d>", fID)))
 		}
-	}
 
+		if i%chunkSize == 0 {
+			s := make([]byte, len(buf.String()))
+			copy(s, []byte(buf.String()))
+			ch <- s
+			buf.Reset()
+			fmt.Printf("Time per NQuad: %v, items in ch: %d\n",
+				int(time.Since(start).Nanoseconds())/(i+1), len(ch))
+		}
+
+	}
+	ch <- nil
+	wg.Wait()
 	if err := w.Flush(); err != nil {
 		panic(err)
 	}
@@ -58,6 +80,7 @@ func main() {
 	if err := f.Close(); err != nil {
 		panic(err)
 	}
+	fmt.Println("Total time taken: ", time.Since(start).Seconds())
 }
 
 func randomNum() int {
@@ -65,9 +88,18 @@ func randomNum() int {
 	return 5 + int(maxDirectFriends*math.Exp(-k*rand.Float64()))
 }
 
-func writeNQuad(w *gzip.Writer, s, p, o string) {
-	str := fmt.Sprintf("<%v> <%v> %v .\n", s, p, o)
-	if _, err := w.Write([]byte(str)); err != nil {
-		panic(err)
+func worker(w *gzip.Writer, ch chan []byte, wg *sync.WaitGroup) {
+	for buf := range ch {
+		if buf == nil {
+			wg.Done()
+			return
+		}
+		if _, err := w.Write(buf); err != nil {
+			panic(err)
+		}
 	}
+}
+
+func getNQuad(s, p, o string) string {
+	return "<" + s + "> <" + p + "> " + o + " .\n"
 }
